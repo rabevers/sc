ba904394ee2c09b882dde080458843008630a3b7
Richard Bevers <rabevers@users.noreply.github.com>
2018-03-31 23:00:40 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,36 +1,113 @@
-;------------------------------------------------------------------------------
-;   Digital I/O example for the LaunchPad
-;   Read the status of built in push button - P1.3
-;      (Note that P1.3 is "1" when the push button is open
-;       and "0" when the button is closed)
-;   Red light if the button is not pushed - P1.0
-;   Green light if the button is pushed - P1.6
-;   Build with Code Composer Studio
-;------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+;
+; Main servo controller program file, based on the TI code composer studio template.
+;
+; Consider this a permanent work in progress. I learn new stuff every day.
+;
+; I started out by reading http://robotics.hobbizine.com/asmlau.html
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430g2553.h"       ; Include device header file
 
-            .cdecls C,LIST,"msp430g2553.h"  ; cdecls tells assembler to allow
-                                            ; the c header file
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section
+            .retainrefs                     ; Additionally retain any sections
+                                            ; that have references to current
+                                            ; section
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
-;------------------------------------------------------------------------------
-;   Main Code
-;------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+                                            ; Main loop here
+;-------------------------------------------------------------------------------
 
-            .text                           ; program start
-            .global _main		    ; define entry point
+;-------------------------------------------------------------------------------
+; Set the clock
+;
+; For now I have chosen a high clock frequency. The idea is to make the code run
+; as fast as it can. That way, theoretically, there is more time available for
+; the communication routines
+;-------------------------------------------------------------------------------
+SetDCO		mov.b CALDCO_16MHZ, DCOCTL		; Moves the value of CALDCO_16MHZ into the register DCOCTL
+											; CALDCO_16MHZ       = 0x10F8; Defined in msp430g2552.cmd
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;|		7		|		6		|		5		|		4		|		3		|		2		|		1		|		0		|
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;| 				 	  DCOx						|							  			MODx									|
+;+----------------------------------------------+-------------------------------------------------------------------------------+
+;|		1		|		1		|		1		|		1		|		1		|		0		|		0		|		0		|
+;+----------------------------------------------+-------------------------------------------------------------------------------+
+;						|								|
+;						|								+ Modulator selection. These bits define how often the f DCO+1 frequency
+;						|								  is used within a period of 32 DCOCLK cycles. During the remaining clock
+;						|								  cycles (32-MOD) the f DCO frequency is used. Not useable when DCOx = 7.
+;						+ 7 (highest possible speed)
 
-_main       mov.w   #0280h,SP               ; initialize stack pointer
-            mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; stop watchdog timer
 
-            bis.b   #11111111b,&P1DIR       ; make P1.0 and P1.6 output
-                                            ; all others are inputs by default
+; The MODx settings according to the docs I could find are not used. This makes me wonder why it has such a specific value.
+; Why not all zeros or ones
 
-Mainloop    bis.b   #11110000b,&P1OUT       ; clear P1.0 (red off)
-            jmp     Mainloop                ; jump to the Mainloop label
+											; CALBC1_16MHZ       = 0x10F9; Defined in msp430g2552.cmd
+			mov.b CALBC1_16MHZ, BCSCTL1		; C examples state something similar but I have not yet found the exact meaning
+											; Moves the value of CALBC1_16MHZ to the BCSCTL1 register
 
-;------------------------------------------------------------------------------
-;   Interrupt Vectors
-;------------------------------------------------------------------------------
+; slau144i msp430x2xx
+; 5.3.2 BCSCTL1, Basic Clock System Control Register 1
+; servoController_linkInfo.xml lists CALBC1_16MHZ as 0x10f9 which is the same as 1000011111001
+; Since we are moving a byte the register would look like this
+;
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;|		7		|		6		|		5		|		4		|		3		|		2		|		1		|		0		|
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;| 	 XT2OFF		|	   XTS		|			  DIVAx				|							  RSELx								|
+;+----------------------------------------------+-------------------------------------------------------------------------------+
+;|		1		|		1		|		1		|		1		|		1		|		0		|		0		|		1		|
+;+----------------------------------------------+-------------------------------------------------------------------------------+
+;		|				|				|								|
+;		|				|				|								+ DEC 9
+;		|				|				|
+;		|				|				+ 11 /8
+;		|				|
+;		|				+ 1 High-frequency mode
+;		|
+;		+- XT2 is off if it is not used for MCLK or SMCLK.
+;
+; The actual value of RSEL seems to set the actual frequence step in the range. I can only assume The settings combined
+; set the DCO to 16Mhz
+
+ConfigureTimerA
+			mov.b 0x00, ; Stop the timer
+; configure the timer
+; Start the timer up again
+; @todo determine if we allow communication interrupts here
+
+
+			; todo check the # in the following two statements, seems to have something to do with addressing modes
+			; a recent error leads me to believe it has something to do with hex numbers
+			; changing pins will be primarily related to servo control
+			; Communication is probably done differently
+			mov.b #0xff, P1DIR				; Set port 1 to be all outputs (high)
+			mov.b #0x7e, P1OUT				; Set all pins to high
+
+;-------------------------------------------------------------------------------
+;           Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect 	.stack
+
+;-------------------------------------------------------------------------------
+;           Interrupt Vectors
+;-------------------------------------------------------------------------------
             .sect   ".reset"                ; MSP430 RESET Vector
-            .short  _main
+            .short  RESET
 
-            .end
+
+; DCOCTL, DCO Control register
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;|		7		|		6		|		5		|		4		|		3		|		2		|		1		|		0		|
+;+--------------+---------------+---------------+---------------+---------------+---------------+---------------+---------------+
+;| 					  DCOx						|									   MODx										|
+;+----------------------------------------------+-------------------------------------------------------------------------------+
